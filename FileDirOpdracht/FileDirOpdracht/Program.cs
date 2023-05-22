6c341ddfbb9d88c9f148608708a8c53c712015b9
Ivo <112894315+IvoBergen@users.noreply.github.com>
2023-05-22 12:04:09 +0200

    Files and directories

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\ivobe\Desktop\Ma\Bewijzenmap\periode1.4\prog4\2023P4ProgCs2");
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files) 
+            { 
+                Console.Write(file.FullName);
+            }
+            DirectoryInfo[] di = dir.GetDirectories();
+            foreach (DirectoryInfo di2 in di) 
+            { 
+                Console.WriteLine(di2.);
+            }
+        }
+    }
+}
